834c64321af5e7d086438ebe727076d9393218d6
Michael Fang <mjfang@stanford.edu>
2017-06-03 08:54:31 +0000

    updating nets

--- run.py
+++ run.py
@@ -7,9 +7,14 @@ from scipy import misc
 from PIL import Image
 
 batch_size = 64
-
-input_width = 2270
+num_train = 100
+num_val = 100
+num_test = 100
+input_width = 2270 #original image sizes
 input_height = 342
+
+input_width_modified = 1000
+scale = 0.25
 
 def get_random_exclude(low, high, exclude):
   r = np.random.randint(low, high)
@@ -19,6 +24,9 @@ def get_random_exclude(low, high, exclude):
 
 #assume
 def processImage(image):
+  image = image[:, :input_width_modified]
+  image = misc.imresize(image,scale)
+  image = np.expand_dims(image,axis=2)
   image = image.astype(float)
   return image / 256.
 #assume
@@ -36,17 +44,10 @@ def get_dataset(data_dir, writer_index_low, writer_index_high, num_examples):
     file_idx1 = np.random.randint(0, num_urls)
     file_idx2 = get_random_exclude(0, num_urls, file_idx1)
 
-    #TODO: load image
-
     image1 = np.reshape(misc.imread(os.path.join(url_folder, urls[file_idx1])), (input_height, input_width))
     image2 = np.reshape(misc.imread(os.path.join(url_folder, urls[file_idx2])), (input_height, input_width))
-    image1 = image1[:, :input_height]
-    image2 = image2[:, :input_height]
-    image1 = misc.imresize(image1, 0.3)
-    image2 = misc.imresize(image2, 0.3)
-    Image.fromarray(image1, 'L').show()
-    image1 = processImage(np.expand_dims(image1, axis=2))
-    image2 = processImage(np.expand_dims(image2, axis=2))
+    image1 = processImage(image1)
+    image2 = processImage(image2)
 
     pairs += [[image1, image2]]
 
@@ -57,19 +58,19 @@ def get_dataset(data_dir, writer_index_low, writer_index_high, num_examples):
     num_urls = len(urls)
     file_idx3 = np.random.randint(0, num_urls)
     image3 = np.reshape(misc.imread(os.path.join(url_folder, urls[file_idx3])), (input_height, input_width))
-    image3 = image3[:, :input_height]
-    image3 = misc.imresize(image3, 0.3)
+    #image3 = image3[:, :input_height]
+    #image3 = misc.imresize(image3, 0.3)
     wr_2 = get_random_exclude(writer_index_low, writer_index_high, wr_1)
     url_folder = os.path.join(data_dir, writer_folders[wr_2])
     urls = os.listdir(url_folder)
     num_urls = len(urls)
     file_idx4 = np.random.randint(0, num_urls)
     image4 = np.reshape(misc.imread(os.path.join(url_folder, urls[file_idx4])), (input_height, input_width))
-    image4 = image4[:, :input_height]
+    #image4 = image4[:, :input_height]
 
-    image4 = misc.imresize(image4, 0.3)
-    image3 = processImage(np.expand_dims(image3, axis=2))
-    image4 = processImage(np.expand_dims(image4, axis=2))
+    #image4 = misc.imresize(image4, 0.3)
+    image3 = processImage(image3)
+    image4 = processImage(image4)
     pairs += [[image3, image4]]
     labels += [1,0]
   return np.array(pairs),np.array(labels)
@@ -89,6 +90,15 @@ def compute_counts(prediction,labels):
   
   return tp, tn, fp, fn
 
+def output_random_distances(prediction, labels, num_outputs = 5):
+  for i in range(num_outputs):
+    idx = np.random.randint(0, labels.shape[0])
+    print(labels[idx], prediction[idx])
+
+def normalize(x, mean):
+  return (x - mean)
+
+
 #657 writers
 #use different writers for training set, validation set, test_set
 def create_data(data_dir, num_train, num_val, num_test):
@@ -102,9 +112,12 @@ def create_data(data_dir, num_train, num_val, num_test):
   cutoff_1 = np.round(train_frac * num_writers)
   cutoff_2 = cutoff_1 + np.round(val_frac * num_writers)
   x_train, y_train = get_dataset(data_dir, 0, cutoff_1, num_train)
+  x_mean = np.mean(x_train, axis=0)
+  x_train = normalize(x_train, x_mean)
   x_val, y_val = get_dataset(data_dir, cutoff_1, cutoff_2, num_val)
+  x_val = normalize(x_val, x_mean)
   x_test, y_test = get_dataset(data_dir, cutoff_2, num_writers, num_test)
-
+  x_test = normalize(x_test, x_mean)
   return (x_train, y_train, x_val, y_val, x_test, y_test)
 
 
@@ -116,7 +129,7 @@ def get_next_batch(start, end, inputs, labels):
 
 
 start_time = time.time()
-x_train, y_train, x_val, y_val, x_test, y_test = create_data("preprocessed_lines_contrast_adjustment", 500, 200, 200)
+x_train, y_train, x_val, y_val, x_test, y_test = create_data("preprocessed_lines_contrast_adjustment", num_train, num_val, num_test)
 print("finished getting data", time.time() - start_time)
 #data
 
@@ -126,8 +139,12 @@ def do_summaries(siamese, graph, output_file = "results/"):
   merged = tf.summary.merge_all()
   return merged, tf.summary.FileWriter(output_file, graph)
 
+
+tr_acc_epoch = []
+val_acc_epoch = []
+
 with tf.Session() as sess:
-  siamese = siamese_net.Siamese_Net(batch_size, [None, input_height, input_height, 1])
+  siamese = siamese_net.Siamese_Net(batch_size, [None, int(input_height*scale), int(input_width_modified * scale), 1])
   
   #train_step = tf.train.AdamOptimizer().minimize(siamese.loss)
   merged, fw = do_summaries(siamese, sess.graph) 
@@ -141,6 +158,11 @@ with tf.Session() as sess:
     count_fn = 0.
     total_batch = int(x_train.shape[0]/batch_size)
     start_time = time.time()
+    perms = np.random.shuffle(np.arange(x_train.shape[0]))
+    s = x_train.shape
+    x_train = np.reshape(x_train[perms, :, :, :], s)
+    y_train =np.squeeze( y_train[perms])
+
     for i in range(total_batch):
       start = i * batch_size
       end = (i+1) * batch_size
@@ -151,6 +173,7 @@ with tf.Session() as sess:
         siamese.y : y
       })
       fw.add_summary(summary, counter)
+      counter += 1
       tr_tp, tr_tn, tr_fp, tr_fn = compute_counts(predict, y)
       count_tp += tr_tp
       count_tn += tr_tn
@@ -166,6 +189,9 @@ with tf.Session() as sess:
     duration = time.time() - start_time
     acc = (count_tp + count_tn) / (count_tp + count_tn + count_fp + count_fn)  
     print('epoch %d  time: %f loss %0.5f acc %0.2f tp %f tn %f fp %f fn %f' %(epoch,duration,avg_loss/(total_batch),acc, count_tp, count_tn, count_fp, count_fn))
+    tr_acc_epoch.append(acc)
+    fw.add_summary(tf.Summary(value=[tf.Summary.Value(tag="tr_acc", simple_value=acc)]), epoch)
+    
     #val
     v_tp = 0.
     v_tn = 0.
@@ -189,7 +215,11 @@ with tf.Session() as sess:
   te_tn = 0.
   te_fp = 0.
   te_fn = 0.
-
+  val_acc = 100 * (v_tp + v_tn) / (v_tp + v_tn + v_fp + v_fn)
+  val_acc_epoch.append(val_acc)
+  print("Val set accuracy %0.2f tp %f tn %f fp %f fn %f" % (val_acc, v_tp, v_tn, v_fp, v_fn))
+  fw.add_summary(tf.Summary(value=[tf.Summary.Value(tag="val_acc", simple_value=val_acc)]), epoch)
+  
   total_test_batch = int(x_test.shape[0]/batch_size)
   for i in range(total_test_batch):
     start = i * batch_size
@@ -207,4 +237,13 @@ with tf.Session() as sess:
     te_fn += fn
 
   print("Test set accuracy %0.2f" % (100 * (te_tp + te_tn) / (te_tp + te_tn + te_fp + te_fn)))
+  print(tr_acc_epoch, val_acc_epoch)
+  #t = np.arange(len(tr_acc_epoch))
+  #plt.plot(t, tr_acc_epoch, 'bo')
+  #plt.plot(t, val_acc_epoch, 'ro')
+  #plt.ylabel('Accuracy')
+  #plt.xlabel('Num. epochs')
+  #tr_patch = mpatches.Patch(color='blue', label='Training Accuracy')
+  #val_patch = mpatches.Patch(color='red', label='Validation Accuracy')
+  #plt.savefig('fig.png')
 
--- siamese_net.py
+++ siamese_net.py
@@ -5,10 +5,11 @@ import tensorflow.contrib.layers as layers
 
 
 class Siamese_Net:
-
+  
   def __init__(self, batch_size, input_shape):
     self.x1 = tf.placeholder(tf.float32, input_shape) # TODO: change to true dimensions
     self.x2 = tf.placeholder(tf.float32, input_shape)
+    self.keep_prob = 0.5
     self.batch_size = batch_size
     with tf.variable_scope("siamese") as scope:
       self.out1 = self.network(self.x1)
@@ -16,26 +17,40 @@ class Siamese_Net:
       self.out2 = self.network(self.x2)
     #L2 distance
     pow = tf.pow(tf.subtract(self.out1,self.out2), 2)
-    self.distance = tf.sqrt(tf.reduce_sum(pow,1,keep_dims=True))
+    #self.distance = tf.sqrt(tf.reduce_sum(pow,1,keep_dims=True))
 
     self.y = tf.placeholder(tf.float32, [None])
-    self.loss = self.contrastive_loss()
+    self.loss = self.softmax_loss()
     self.train_op = self.add_optimizer_loss(self.loss)
   def network(self, input):
-    conv1 = layers.conv2d(input, num_outputs=32, kernel_size=[24, 24], stride=[2, 2])
-    pool1 = tf.nn.max_pool(conv1,[1,4,4,1], strides = [1,3,3,1], padding='VALID') 
-    conv2 = layers.conv2d(pool1, num_outputs=16, kernel_size=[9, 9], stride=[1,1])
+    self.regularizer = layers.l2_regularizer(scale=0.1)
+    conv1 = layers.conv2d(input, num_outputs=32, kernel_size=[10, 10], stride=[1, 1])
+    pool1 = tf.nn.max_pool(conv1,[1,2,2,1], strides = [1,2,2,1], padding='VALID') 
+    conv2 = layers.conv2d(pool1, num_outputs=64, kernel_size=[8, 8], stride=[1,1])
+    pool2 = tf.nn.max_pool(conv2,[1,2,2,1], strides = [1,2,2,1], padding='VALID') 
+    conv3 = layers.conv2d(pool2, num_outputs = 64, kernel_size=[4,4], stride=[1,1])
     conv2_flattened = layers.flatten(conv2)
-    fc1 = layers.fully_connected(conv2_flattened, 256, biases_initializer=tf.constant_initializer(0))
-    out = layers.fully_connected(fc1, 2, activation_fn=None)
+    fc1 = layers.fully_connected(conv2_flattened, 400, biases_initializer=tf.constant_initializer(0), weights_regularizer=self.regularizer)
+    d_fc1 = tf.nn.dropout(fc1,self.keep_prob)
+    out = layers.fully_connected(d_fc1, 200, weights_regularizer = self.regularizer)
+    #d_out = tf.nn.dropout(out, self.keep_prob)
     return out
-
+  def softmax_loss(self):
+    self.h = tf.concat([tf.square(self.out1-self.out2), self.out1, self.out2], axis=1)
+    out = layers.fully_connected(self.h, 2, activation_fn = None)
+    
+    self.distance =tf.nn.softmax( out)[:, 0]
+    y_int = tf.cast(self.y, tf.int32)
+    reg_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_term = layers.apply_regularization(self.regularizer, reg_vars)
+    return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_int, logits=out)) + reg_term
+    
   def contrastive_loss(self):
     a = self.y * tf.square(self.distance)
     a2 = (1-self.y) * tf.square(tf.maximum((1-self.distance), 0))
     return tf.reduce_sum(a + a2) / self.batch_size / 2
   def add_optimizer_loss(self, loss):
-    optimizer = tf.train.AdamOptimizer()
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
     gvs = optimizer.compute_gradients(loss)
     gs, vs = zip(*gvs)
     self.grad_norm = tf.global_norm(gs)
