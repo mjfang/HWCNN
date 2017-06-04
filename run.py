9bccc084d82c26be0fe300337e72e29eee67b1c9
Margaret Shen <marshen@stanford.edu>
2017-06-04 14:56:08 -0700

    VGG merged w Siamese nets python file

--- run.py
+++ run.py
@@ -10,8 +10,9 @@ batch_size = 20
 num_train = 10000
 num_val = 1000
 num_test = 1000
-input_width = 375 #original image sizes
-input_height = 85
+input_width = 250 #original image sizes
+input_height = 100
+data_dir = "preprocessed_words"
 
 input_width_modified = 375
 scale = 1
@@ -44,7 +45,7 @@ def get_dataset(data_dir, writer_index_low, writer_index_high, num_examples):
     file_idx1 = np.random.randint(0, num_urls)
     file_idx2 = get_random_exclude(0, num_urls, file_idx1)
 
-    
+
     image1 = np.reshape(misc.imread(os.path.join(url_folder, urls[file_idx1])), (input_height, input_width))
     image2 = np.reshape(misc.imread(os.path.join(url_folder, urls[file_idx2])), (input_height, input_width))
     image1 = processImage(image1)
@@ -83,12 +84,12 @@ def get_dataset(data_dir, writer_index_low, writer_index_high, num_examples):
 #
 #
 def compute_counts(prediction,labels):
-  
+
   tp = labels[prediction.ravel() < 0.5].sum()
   tn = (1-labels)[prediction.ravel() > 0.5].sum()
   fp = labels[prediction.ravel() > 0.5].sum()
   fn = (1-labels)[prediction.ravel() < 0.5].sum()
-  
+
   return tp, tn, fp, fn
 
 def output_random_distances(prediction, labels, num_outputs = 5):
@@ -130,7 +131,7 @@ def get_next_batch(start, end, inputs, labels):
 
 
 start_time = time.time()
-x_train, y_train, x_val, y_val, x_test, y_test = create_data("preprocessed_words_padding_segmented", num_train, num_val, num_test)
+x_train, y_train, x_val, y_val, x_test, y_test = create_data(data_dir, num_train, num_val, num_test)
 print("finished getting data", time.time() - start_time)
 #data
 
@@ -146,9 +147,9 @@ val_acc_epoch = []
 
 with tf.Session() as sess:
   siamese = siamese_net.Siamese_Net(batch_size, [None, int(input_height*scale), int(input_width_modified * scale), 1])
-  
+
   #train_step = tf.train.AdamOptimizer().minimize(siamese.loss)
-  merged, fw = do_summaries(siamese, sess.graph) 
+  merged, fw = do_summaries(siamese, sess.graph)
   sess.run(tf.global_variables_initializer())
   counter = 0
   for epoch in range(30):
@@ -188,11 +189,11 @@ with tf.Session() as sess:
       #if i % 10 == 0:
       #  ('step %d: loss %.3f' % (i, loss_v))
     duration = time.time() - start_time
-    acc = (count_tp + count_tn) / (count_tp + count_tn + count_fp + count_fn)  
+    acc = (count_tp + count_tn) / (count_tp + count_tn + count_fp + count_fn)
     print('epoch %d  time: %f loss %0.5f acc %0.2f tp %f tn %f fp %f fn %f' %(epoch,duration,avg_loss/(total_batch),acc, count_tp, count_tn, count_fp, count_fn))
     tr_acc_epoch.append(acc)
     fw.add_summary(tf.Summary(value=[tf.Summary.Value(tag="tr_acc", simple_value=acc)]), epoch)
-    
+
     #val
     v_tp = 0.
     v_tn = 0.
@@ -220,11 +221,11 @@ with tf.Session() as sess:
   val_acc_epoch.append(val_acc)
   print("Val set accuracy %0.2f tp %f tn %f fp %f fn %f" % (val_acc, v_tp, v_tn, v_fp, v_fn))
   fw.add_summary(tf.Summary(value=[tf.Summary.Value(tag="val_acc", simple_value=val_acc)]), epoch)
-  
+
   total_test_batch = int(x_test.shape[0]/batch_size)
   for i in range(total_test_batch):
     start = i * batch_size
-    end = (i+1) * batch_size 
+    end = (i+1) * batch_size
     input1, input2, y = get_next_batch(start, end, x_test, y_test)
     print(y.shape)
     predict = siamese.distance.eval(feed_dict={
--- siamese_net.py
+++ siamese_net.py
@@ -5,7 +5,7 @@ import tensorflow.contrib.layers as layers
 
 
 class Siamese_Net:
-  
+
   def __init__(self, batch_size, input_shape):
     self.x1 = tf.placeholder(tf.float32, input_shape) # TODO: change to true dimensions
     self.x2 = tf.placeholder(tf.float32, input_shape)
@@ -22,33 +22,135 @@ class Siamese_Net:
     self.y = tf.placeholder(tf.float32, [None])
     self.loss = self.softmax_loss()
     self.train_op = self.add_optimizer_loss(self.loss)
+
   def network(self, input):
-    self.regularizer = layers.l2_regularizer(scale=0.1)
-    conv1 = layers.conv2d(input, num_outputs=32, kernel_size=[10, 10], stride=[1, 1])
-    pool1 = tf.nn.max_pool(conv1,[1,2,2,1], strides = [1,2,2,1], padding='VALID') 
-    conv2 = layers.conv2d(pool1, num_outputs=64, kernel_size=[8, 8], stride=[1,1])
-    pool2 = tf.nn.max_pool(conv2,[1,2,2,1], strides = [1,2,2,1], padding='VALID') 
-    conv3 = layers.conv2d(pool2, num_outputs = 64, kernel_size=[4,4], stride=[1,1])
-    conv2_flattened = layers.flatten(conv2)
-    fc1 = layers.fully_connected(conv2_flattened, 400, biases_initializer=tf.constant_initializer(0), weights_regularizer=self.regularizer)
-    d_fc1 = tf.nn.dropout(fc1,self.keep_prob)
-    out = layers.fully_connected(d_fc1, 200, weights_regularizer = self.regularizer)
+    # self.regularizer = layers.l2_regularizer(scale=0.1)
+    # conv1 = layers.conv2d(input, num_outputs=32, kernel_size=[10, 10], stride=[1, 1])
+    # pool1 = tf.nn.max_pool(conv1,[1,2,2,1], strides = [1,2,2,1], padding='VALID')
+    # conv2 = layers.conv2d(pool1, num_outputs=64, kernel_size=[8, 8], stride=[1,1])
+    # pool2 = tf.nn.max_pool(conv2,[1,2,2,1], strides = [1,2,2,1], padding='VALID')
+    # conv3 = layers.conv2d(pool2, num_outputs = 64, kernel_size=[4,4], stride=[1,1])
+    # conv2_flattened = layers.flatten(conv2)
+    # fc1 = layers.fully_connected(conv2_flattened, 400, biases_initializer=tf.constant_initializer(0), weights_regularizer=self.regularizer)
+    # d_fc1 = tf.nn.dropout(fc1,self.keep_prob)
+    # out = layers.fully_connected(d_fc1, 200, weights_regularizer = self.regularizer)
+
     #d_out = tf.nn.dropout(out, self.keep_prob)
-    return out
+
+    self.conv1_1 = self.conv_layer(input, 1, 64, "conv1_1") # should only be one channel for b/w?
+    self.conv1_2 = self.conv_layer(self.conv1_1, 64, 64, "conv1_2")
+    self.pool1 = self.max_pool(self.conv1_2, 'pool1')
+
+    self.conv2_1 = self.conv_layer(self.pool1, 64, 128, "conv2_1")
+    self.conv2_2 = self.conv_layer(self.conv2_1, 128, 128, "conv2_2")
+    self.pool2 = self.max_pool(self.conv2_2, 'pool2')
+
+    self.conv3_1 = self.conv_layer(self.pool2, 128, 256, "conv3_1")
+    self.conv3_2 = self.conv_layer(self.conv3_1, 256, 256, "conv3_2")
+    self.conv3_3 = self.conv_layer(self.conv3_2, 256, 256, "conv3_3")
+    self.conv3_4 = self.conv_layer(self.conv3_3, 256, 256, "conv3_4")
+    self.pool3 = self.max_pool(self.conv3_4, 'pool3')
+
+    self.conv4_1 = self.conv_layer(self.pool3, 256, 512, "conv4_1")
+    self.conv4_2 = self.conv_layer(self.conv4_1, 512, 512, "conv4_2")
+    self.conv4_3 = self.conv_layer(self.conv4_2, 512, 512, "conv4_3")
+    self.conv4_4 = self.conv_layer(self.conv4_3, 512, 512, "conv4_4")
+    self.pool4 = self.max_pool(self.conv4_4, 'pool4')
+
+    self.conv5_1 = self.conv_layer(self.pool4, 512, 512, "conv5_1")
+    self.conv5_2 = self.conv_layer(self.conv5_1, 512, 512, "conv5_2")
+    self.conv5_3 = self.conv_layer(self.conv5_2, 512, 512, "conv5_3")
+    self.conv5_4 = self.conv_layer(self.conv5_3, 512, 512, "conv5_4")
+    self.pool5 = self.max_pool(self.conv5_4, 'pool5')
+
+    self.fc6 = self.fc_layer(self.pool5, 25088, 4096, "fc6")  # 25088 = ((224 // (2 ** 5)) ** 2) * 512
+    self.relu6 = tf.nn.relu(self.fc6)
+    self.relu6 = tf.nn.dropout(self.relu6, self.dropout)
+
+    self.fc7 = self.fc_layer(self.relu6, 4096, 4096, "fc7")
+    self.relu7 = tf.nn.relu(self.fc7)
+    self.relu7 = tf.nn.dropout(self.relu7, self.dropout)
+
+    self.fc8 = self.fc_layer(self.relu7, 4096, 1000, "fc8")
+
+    return self.fc8
+
+  def avg_pool(self, bottom, name):
+    return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+
+  def max_pool(self, bottom, name):
+    return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+
+  def conv_layer(self, bottom, in_channels, out_channels, name):
+    with tf.variable_scope(name):
+      filt, conv_biases = self.get_conv_var(3, in_channels, out_channels, name)
+
+      conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
+      bias = tf.nn.bias_add(conv, conv_biases)
+      relu = tf.nn.relu(bias)
+
+      return relu
+
+  def fc_layer(self, bottom, in_size, out_size, name):
+    with tf.variable_scope(name):
+      weights, biases = self.get_fc_var(in_size, out_size, name)
+
+      x = tf.reshape(bottom, [-1, in_size])
+      fc = tf.nn.bias_add(tf.matmul(x, weights), biases)
+
+      return fc
+
+  def get_conv_var(self, filter_size, in_channels, out_channels, name):
+      initial_value = tf.truncated_normal([filter_size, filter_size, in_channels, out_channels], 0.0, 0.001)
+      filters = self.get_var(initial_value, name, 0, name + "_filters")
+
+      initial_value = tf.truncated_normal([out_channels], .0, .001)
+      biases = self.get_var(initial_value, name, 1, name + "_biases")
+
+      return filters, biases
+
+  def get_fc_var(self, in_size, out_size, name):
+      initial_value = tf.truncated_normal([in_size, out_size], 0.0, 0.001)
+      weights = self.get_var(initial_value, name, 0, name + "_weights")
+
+      initial_value = tf.truncated_normal([out_size], .0, .001)
+      biases = self.get_var(initial_value, name, 1, name + "_biases")
+
+      return weights, biases
+
+  def get_var(self, initial_value, name, idx, var_name):
+      if self.data_dict is not None and name in self.data_dict:
+          value = self.data_dict[name][idx]
+      else:
+          value = initial_value
+
+      if self.trainable:
+          var = tf.Variable(value, name=var_name)
+      else:
+          var = tf.constant(value, dtype=tf.float32, name=var_name)
+
+      self.var_dict[(name, idx)] = var
+
+      # print var_name, var.get_shape().as_list()
+      assert var.get_shape() == initial_value.get_shape()
+
+      return var
+
   def softmax_loss(self):
     self.h = tf.concat([tf.square(self.out1-self.out2), self.out1, self.out2], axis=1)
     out = layers.fully_connected(self.h, 2, activation_fn = None)
-    
+
     self.distance =tf.nn.softmax( out)[:, 0]
     y_int = tf.cast(self.y, tf.int32)
     reg_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
     reg_term = layers.apply_regularization(self.regularizer, reg_vars)
     return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_int, logits=out)) + reg_term
-    
+
   def contrastive_loss(self):
     a = self.y * tf.square(self.distance)
     a2 = (1-self.y) * tf.square(tf.maximum((1-self.distance), 0))
     return tf.reduce_sum(a + a2) / self.batch_size / 2
+
   def add_optimizer_loss(self, loss):
     optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
     gvs = optimizer.compute_gradients(loss)
