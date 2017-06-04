b5b11b1d6d8cd51e89e3840ea3bfdff24e4f9783
William Du <willadu@stanford.edu>
2017-06-04 20:49:54 +0000

    processes words

--- 231n_proj_data_preprocessing.py
+++ 231n_proj_data_preprocessing.py
@@ -4,6 +4,8 @@ from scipy import misc
 import numpy as np
 import glob
 import os
+import pickle
+from math import floor, ceil
 
 def create_form_to_author_map(schema_file):
 	form_to_author = {}
@@ -26,18 +28,29 @@ def find_largest_image_dimension(png_root_dir):
 		largest_image_height = int(f.readline())
 		largest_image_width = int(f.readline())
 	else:
+		image_widths = []
+		image_heights = []
 		largest_image_height = 0
 		largest_image_width = 0
 		for folder in glob.glob(png_root_dir):
 			sub_folder = folder + "/*"
 			for author_folder in glob.glob(sub_folder):
 				for line_png_file in glob.glob(author_folder + "/*.png"):
-					line_png_matrix = misc.imread(line_png_file)
+					try:
+						line_png_matrix = misc.imread(line_png_file)
+					except:
+						continue
 					height, width = line_png_matrix.shape
+					image_heights.append(height)
+					image_widths.append(width)
 					if height > largest_image_height:
 						largest_image_height = height
 					if width > largest_image_width:
 						largest_image_width = width
+		widths = open('widths.pkl', 'wb')
+		pickle.dump(image_widths, widths)
+		heights = open('heights.pkl', 'wb')
+		pickle.dump(image_heights, heights)
 	return largest_image_height, largest_image_width
 
 def pad_original_png_images(png_root_dir, preprocessed_pngs_folder, largest_height, largest_width, form_to_author_map):
@@ -45,15 +58,29 @@ def pad_original_png_images(png_root_dir, preprocessed_pngs_folder, largest_heig
 		sub_folder = folder + "/*"
 		for form_folder_path in glob.glob(sub_folder):
 			for line_png_file in glob.glob(form_folder_path + "/*.png"):
-				line_png_matrix = misc.imread(line_png_file)
-				# binary = line_png_matrix > 200
-				# line_png_matrix[binary] = 255
+				try:
+					line_png_matrix = misc.imread(line_png_file)
+				except:
+					continue
+				binary = line_png_matrix > 200
+				line_png_matrix[binary] = 255
 				height, width = line_png_matrix.shape
-				height_padding_needed = largest_height - height
-				width_padding_needed = largest_width - width
-				pad_width_top_bottom = (height_padding_needed/2, height_padding_needed - (height_padding_needed/2))
-				pad_width_left_right = (0, width_padding_needed)
-				padded_image = np.pad(line_png_matrix, (pad_width_top_bottom, pad_width_left_right), 'constant', constant_values=255)
+				padded_image = line_png_matrix
+				if height < 4 or width < 4:
+					continue						
+				if height <= largest_height:
+					height_padding_needed = largest_height - height
+					pad_width_top_bottom = (int(floor(height_padding_needed/2)), int(ceil(height_padding_needed - (height_padding_needed/2))))
+					padded_image = np.pad(line_png_matrix, (pad_width_top_bottom, (0,0)), 'constant', constant_values=255)
+				else:
+					padded_image = padded_image[0:largest_height,]
+				if width <= largest_width:	
+					width_padding_needed = int(largest_width - width)
+					pad_width_left_right = (0, width_padding_needed)
+					padded_image = np.pad(line_png_matrix, ((0,0), pad_width_left_right), 'constant', constant_values=255)
+				else:
+					padded_image = padded_image[:, 0:largest_width]
+				padded_image = misc.imresize(padded_image,0.25)
 				save_preprocessed_image(padded_image, line_png_file, form_to_author_map, preprocessed_pngs_folder)
 
 def save_preprocessed_image(image_array, line_png_file, form_to_author_map, preprocessed_pngs_folder):
@@ -66,18 +93,11 @@ def save_preprocessed_image(image_array, line_png_file, form_to_author_map, prep
 	destination = destination_folder + "/" + file_name
 	misc.imsave(destination, image_array)
 
-preprocessed_pngs_folder = "preprocessed_lines_padding_only"
-schema_file = "meta.txt"
-raw_images_root_dir = "lines/*"
+preprocessed_pngs_folder = "preprocessed_words"
+schema_file = "forms.txt"
+raw_images_root_dir = "words/*"
 form_to_author_map = create_form_to_author_map(schema_file)
 create_folder_for_preprocessed_images(preprocessed_pngs_folder)
 largest_height, largest_width = find_largest_image_dimension(raw_images_root_dir)
 pad_original_png_images(raw_images_root_dir, preprocessed_pngs_folder, largest_height, largest_width, form_to_author_map)
-
-
-
-
-
-
-
 
